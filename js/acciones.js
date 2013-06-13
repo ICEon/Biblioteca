498a64767ae7e025582bf097b4e0594ec2ba190c
ICEon <matrix_eon@yahoo.com>
2013-06-13 07:57:44 -0400

    Update acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -28,7 +28,8 @@ alert (formulario.attr('name'));
 	{
 		
 			case 'log':
-
+var bla = $('input[usuario]').val();
+alert (bla);
 			var usuario = formulario.children('input:eq(0)').val();
 			var password = formulario.children('input:eq(1)').val();
 							alert (usuario);
