f39d083fff113f6d594e0af3506c88f58df630ba
Eon Matrix <matrix_eon@yahoo.com>
2013-06-13 06:21:01 -0500

    Biblioteca
    Aplicacion de biblioteca commit 1

---
+++ js/acciones.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+function login(U,P){
+	datos = "usuario="+U+"&passwrod="+P;
+	$.ajax({
+		type: "POST",
+		url: "192.168.1.69/Biblioteca_/Log_in_m.php",
+		data: datos
+	}).done(function(msg) {
+		alert (msg);
+        if(msg == "" || msg == null)
+         {
+          alert ("Nombre de usuario y/o password incorrecto");
+         }
+        else
+         {
+  	      alert ("Bienvenido");
+         }
+
+	});
+ } //login
+ 
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", function(){
+			$('.Send').tap(function(){
+		var formulario = $(this).parents('form');
+
+	switch(formulario.attr('name'))
+	{
+		
+			case 'log':
+
+			var usuario = formulario.children('input:eq(0)').val();
+			var password = formulario.children('input:eq(1)').val();
+				login(usuario,password);
+	
+				break;	
+				
+
+		}
+	});//tap	
+	});//device
+});//document
